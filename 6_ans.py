7a0237e8d8a820e0718143a11f72477040bd6c42
Fazle Rabbi <142036307+Fazle-Rabbi72@users.noreply.github.com>
2024-04-24 01:27:05 +0600

    6_ans.py

--- 6_ans.py
+++ 6_ans.py
@@ -1,7 +1,13 @@
-def view_available_seats(self, id):
-        
-        print("Available Seats:")
-        for row in range(self.rows):
-            for col in range(self.cols):
-                if self.seats[id][row][col] == '0':
-                    print(f"Seat: ({row},{col})")
+def view_available_seats(self, show_id):
+        if show_id not in self.seats:
+            print("Invalid show ID.")
+            return
+
+        print(f"Available seats for Show ID {show_id}:")
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.seats[show_id][i][j]:
+                    print("|O|", end=" ")
+                else:
+                    print("(B)", end=" ")
+            print()
