38309a1877641c7bbef86a768365a7b54031e7b2
Fazle Rabbi <142036307+Fazle-Rabbi72@users.noreply.github.com>
2024-04-24 01:23:24 +0600

    Update 3_ans.py

--- 3_ans.py
+++ 3_ans.py
@@ -1,4 +1,3 @@
 def entry_show(self, id, movie_name, time):
-
-    self.show_list.append((id, movie_name, time))
-    self.seats[id] = [['0' for i in range(self.cols)] for j in range(self.rows)]  
+        self.show_list.append((id, movie_name, time))
+        self.seats[id] = [[True for i in range(self.cols)] for j in range(self.rows)]
