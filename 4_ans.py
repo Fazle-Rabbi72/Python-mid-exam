43c72e5ea8e33af199c962ed84d91bc174bf880c
Fazle Rabbi <142036307+Fazle-Rabbi72@users.noreply.github.com>
2024-04-24 01:24:34 +0600

    4_ans.py

--- 4_ans.py
+++ 4_ans.py
@@ -1,11 +1,16 @@
-def book_seats(self, id, seat_list):
-        if id not in [show[0] for show in self.show_list]:
-            print("Invalid ID")
+def book_seats(self, show_id, seat_list):
+        if show_id not in self.seats:
+            print("Invalid show ID.")
             return
+
         for seat in seat_list:
             row, col = seat
-            if self.seats[id][row][col] != '0':
-                print(f"Seat ({row},{col}) is already booked!")
+            if row < 0 or row >= self.rows or col < 0 or col >= self.cols:
+                print("No Existing Seat For booking")
+                continue
+
+            if not self.seats[show_id][row][col]:
+                print(f"Seat {row},{col} is already booked.")
             else:
-                self.seats[id][row][col] = 'Booked'
-        print("Seats booked successfully!")
+                self.seats[show_id][row][col] = False
+                print(f"Seat {row},{col} booked successfully.")
