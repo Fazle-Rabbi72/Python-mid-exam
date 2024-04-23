5490893c5deca49f9684e59ec4e2d5a4db29e8b0
Fazle Rabbi <142036307+Fazle-Rabbi72@users.noreply.github.com>
2024-04-24 01:22:38 +0600

    Update 2_ans.py

--- 2_ans.py
+++ 2_ans.py
@@ -1,10 +1,9 @@
 class Hall:
     def __init__(self, rows, cols, hall_no):
-        
         self.rows = rows
         self.cols = cols
         self.hall_no = hall_no
-        self.seats = {}  
-        self.show_list = [] 
+        self.seats = {}
+        self.show_list = []
 
-        Star_Cinema.entry_hall(self)
+        Star_Cinema().entry_hall(self)
